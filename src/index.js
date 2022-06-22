6b77e92616730d48a9964a1e21cccb239af4edfc
PrideByte <alpha5.2@mail.ru>
2022-06-22 09:02:56 +0300

    fix: minor parentheses fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (m) {
-	return (m && m.reduce((acc, el, i) => acc = (!(i % 2)) ? [...acc, ...el]: [...acc, ...el.reverse()], [])) || [];
+	return m && m.reduce((acc, el, i) => acc = (!(i % 2)) ? [...acc, ...el]: [...acc, ...el.reverse()], []) || [];
 }
